82e0fc151047e83751c3d6921c14797e106a54e2
Dunura-Rathnayaka <Dunurarathnayake23@gmail.com>
2024-06-26 07:46:04 +0100

    Updates

---
+++ precache-manifest.5be93500567cb14cb161ee9304a3a56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b4becead3bffb9e5af85ba2328ed04",
+    "url": "/Moodify-React/index.html"
+  },
+  {
+    "revision": "c9f827e8676b46e26296",
+    "url": "/Moodify-React/static/css/main.e176a13a.chunk.css"
+  },
+  {
+    "revision": "97efa9cc01c48b6ec1ee",
+    "url": "/Moodify-React/static/js/2.5833e8a6.chunk.js"
+  },
+  {
+    "revision": "e26de4f0e0d98f1626b38316e0457409",
+    "url": "/Moodify-React/static/js/2.5833e8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f827e8676b46e26296",
+    "url": "/Moodify-React/static/js/main.81c63ae4.chunk.js"
+  },
+  {
+    "revision": "d32870cb87a022975aba",
+    "url": "/Moodify-React/static/js/runtime-main.fe3c39ca.js"
+  }
+]);
